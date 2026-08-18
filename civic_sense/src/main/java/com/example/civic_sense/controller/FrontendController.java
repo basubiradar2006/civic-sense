f10e26c4c90bdc5e5f40228ccf526c8e55521783
basubiradar2006 <biradarbasu206@gmail.com>
2026-08-18 23:09:24 +0530

    Fix properties file

--- civic_sense/src/main/java/com/example/civic_sense/controller/FrontendController.java
+++ civic_sense/src/main/java/com/example/civic_sense/controller/FrontendController.java
@@ -1,4 +1,13 @@
 package com.example.civic_sense.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FrontendController {
+
+    @GetMapping(value = {"/", "/login", "/register", "/citizen", "/officer", "/contractor", "/complaint"})
+    public String frontend() {
+        return "forward:/index.html";
+    }
 }
